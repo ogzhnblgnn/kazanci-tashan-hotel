d5e297c376f36c2f3dbe2624b3d908559f5c7d35
Oğuzhan Bilgin <ogzhnbxx@gmail.com>
2023-04-08 23:22:29 +0300

    init

---
+++ src/pages/Landing.vue
@@ -0,0 +1,226 @@
+<template>
+  <div>
+    <div class="page-header page-header-small">
+      <parallax
+        class="page-header-image"
+        style="background-image: url('img/download.jpg')"
+      >
+      </parallax>
+      <div class="content-center">
+        <div class="container">
+          <h1 class="title">Kazancı Taşhan Hotel</h1>
+          <div class="text-center">
+            <a href="#pablo" class="btn btn-primary btn-icon btn-round">
+              <i class="fab fa-facebook-square"></i>
+            </a>
+            <a href="#pablo" class="btn btn-primary btn-icon btn-round">
+              <i class="fab fa-twitter"></i>
+            </a>
+            <a href="#pablo" class="btn btn-primary btn-icon btn-round">
+              <i class="fab fa-google-plus"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section section-about-us">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 ml-auto mr-auto text-center">
+            <h2 class="title">Who we are?</h2>
+            <h5 class="description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed justo nibh, feugiat sit amet gravida in, cursus vitae mi. Aenean placerat orci at tincidunt bibendum. Morbi erat nibh, fermentum eget faucibus eget, ornare in dui. Nulla rhoncus hendrerit eros, in auctor eros cursus quis. Ut sagittis fermentum mauris vitae varius. Morbi pretium eleifend lorem nec facilisis. Donec at nunc varius, fringilla dolor sed, eleifend lectus. Nulla porta massa eu nunc vehicula posuere. Sed gravida aliquet orci luctus placerat. Quisque varius purus et porta posuere. Nulla tincidunt porta nunc nec pretium. Duis bibendum lacinia nisi a placerat. Vestibulum suscipit scelerisque aliquet. Quisque quis tempor nulla.
+            </h5>
+          </div>
+        </div>
+        <div class="separator separator-primary"></div>
+        <div class="section-story-overview">
+          <div class="row">
+            <div class="col-md-6">
+              <div
+                class="image-container image-left"
+                style="background-image: url('img/2.jpg')"
+              >
+                <!-- First image on the left side -->
+                <p class="blockquote blockquote-primary">
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed justo nibh, feugiat sit amet gravida in, cursus vitae mi. Aenean placerat orci at tincidunt bibendum."
+                  <br />
+                  <br />
+                  <small>-NOAA</small>
+                </p>
+              </div>
+              <!-- Second image on the left side of the article -->
+              <div
+                class="image-container"
+                style="background-image: url('img/3.jpg')"
+              ></div>
+            </div>
+            <div class="col-md-5">
+              <!-- First image on the right side, above the article -->
+              <div
+                class="image-container image-right"
+                style="background-image: url('img/3.jpg')"
+              ></div>
+              <h3>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed justo nibh, feugiat sit amet gravida in, cursus vitae mi.
+              </h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed justo nibh, feugiat sit amet gravida in, cursus vitae mi. Aenean placerat orci at tincidunt bibendum.
+              </p>
+              <p>
+                Integer sed elementum nisl. Nulla bibendum quam mauris, vel posuere lectus convallis eget. Fusce consequat leo eros, a tempor tortor pharetra non. Suspendisse blandit quam sit amet semper tempus. Aliquam erat volutpat. Vestibulum in lacus ex. Pellentesque sodales iaculis urna a eleifend.
+              </p>
+              <p>
+                Nunc faucibus metus sed lectus tempor, in pulvinar diam maximus. Duis vitae odio sed mi porttitor dictum vel at arcu. Nullam accumsan dolor in lacus semper consequat accumsan sed urna. Praesent in ex a orci ultricies venenatis. Morbi pretium massa eget vestibulum cursus. Suspendisse sollicitudin, libero eget porttitor feugiat, libero leo interdum sapien, id tempor ante tortor a magna.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section section-team text-center">
+      <div class="container">
+        <h2 class="title">Here is our team</h2>
+        <div class="team">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="team-player">
+                <img
+                  src="img/avatar.jpg"
+                  alt="Thumbnail Image"
+                  class="rounded-circle img-fluid img-raised"
+                />
+                <h4 class="title">Romina Hadid</h4>
+                <p class="category text-primary">Model</p>
+                <p class="description">
+                  You can write here details about one of your team members. You
+                  can give more details about what they do. Feel free to add
+                  some <a href="#">links</a> for people to be able to follow
+                  them outside the site.
+                </p>
+                <a href="#pablo" class="btn btn-primary btn-icon btn-round"
+                  ><i class="fab fa-twitter"></i
+                ></a>
+                <a href="#pablo" class="btn btn-primary btn-icon btn-round"
+                  ><i class="fab fa-instagram"></i
+                ></a>
+                <a href="#pablo" class="btn btn-primary btn-icon btn-round"
+                  ><i class="fab fa-facebook-square"></i
+                ></a>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="team-player">
+                <img
+                  src="img/ryan.jpg"
+                  alt="Thumbnail Image"
+                  class="rounded-circle img-fluid img-raised"
+                />
+                <h4 class="title">Ryan Tompson</h4>
+                <p class="category text-primary">Designer</p>
+                <p class="description">
+                  You can write here details about one of your team members. You
+                  can give more details about what they do. Feel free to add
+                  some <a href="#">links</a> for people to be able to follow
+                  them outside the site.
+                </p>
+                <a href="#pablo" class="btn btn-primary btn-icon btn-round"
+                  ><i class="fab fa-twitter"></i
+                ></a>
+                <a href="#pablo" class="btn btn-primary btn-icon btn-round"
+                  ><i class="fab fa-linkedin"></i
+                ></a>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="team-player">
+                <img
+                  src="img/eva.jpg"
+                  alt="Thumbnail Image"
+                  class="rounded-circle img-fluid img-raised"
+                />
+                <h4 class="title">Eva Jenner</h4>
+                <p class="category text-primary">Fashion</p>
+                <p class="description">
+                  You can write here details about one of your team members. You
+                  can give more details about what they do. Feel free to add
+                  some <a href="#">links</a> for people to be able to follow
+                  them outside the site.
+                </p>
+                <a href="#pablo" class="btn btn-primary btn-icon btn-round"
+                  ><i class="fab fa-google-plus"></i
+                ></a>
+                <a href="#pablo" class="btn btn-primary btn-icon btn-round"
+                  ><i class="fab fa-youtube"></i
+                ></a>
+                <a href="#pablo" class="btn btn-primary btn-icon btn-round"
+                  ><i class="fab fa-twitter"></i
+                ></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section section-contact-us text-center">
+      <div class="container">
+        <h2 class="title">Contact Us</h2>
+        <p class="description">Your opinion is important to us</p>
+        <div class="row">
+          <div class="col-lg-6 text-center ml-auto mr-auto col-md-8">
+            <fg-input
+              class="input-lg"
+              placeholder="First Name..."
+              v-model="form.firstName"
+              addon-left-icon="now-ui-icons users_circle-08"
+            >
+            </fg-input>
+            <fg-input
+              class="input-lg"
+              placeholder="Email Here..."
+              v-model="form.email"
+              addon-left-icon="now-ui-icons ui-1_email-85"
+            >
+            </fg-input>
+            <div class="textarea-container">
+              <textarea
+                class="form-control"
+                name="name"
+                rows="4"
+                cols="80"
+                v-model="form.message"
+                placeholder="Type a message..."
+              ></textarea>
+            </div>
+            <div class="send-button">
+              <n-button type="primary" round block size="lg"
+                >Send Message</n-button
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { Button, FormGroupInput } from '@/components';
+export default {
+  name: 'landing',
+  bodyClass: 'landing-page',
+  components: {
+    [Button.name]: Button,
+    [FormGroupInput.name]: FormGroupInput
+  },
+  data() {
+    return {
+      form: {
+        firstName: '',
+        email: '',
+        message: ''
+      }
+    };
+  }
+};
+</script>
+<style></style>
